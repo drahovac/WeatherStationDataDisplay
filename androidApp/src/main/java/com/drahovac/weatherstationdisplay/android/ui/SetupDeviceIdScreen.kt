0cb8e19ec7911d51783b486df710d5eb56e4d4ca
Vaclav Drahorad <drahorad.vac@gmail.com>
2023-07-16 23:07:55 +0200

    Add icons

--- androidApp/src/main/java/com/drahovac/weatherstationdisplay/android/ui/SetupDeviceIdScreen.kt
+++ androidApp/src/main/java/com/drahovac/weatherstationdisplay/android/ui/SetupDeviceIdScreen.kt
@@ -2,12 +2,16 @@ package com.drahovac.weatherstationdisplay.android.ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -17,10 +21,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.BottomEnd
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.drahovac.weatherstationdisplay.MR
+import com.drahovac.weatherstationdisplay.android.R
 
 @Composable
 fun SetupDeviceIdScreen() {
@@ -71,15 +77,29 @@ fun SetupDeviceIdScreen() {
                 color = MaterialTheme.colorScheme.onPrimaryContainer
             )
 
-            OutlinedButton(onClick = { /*TODO*/ }) {
+            OutlinedButton(
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                onClick = { /*TODO*/ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_language_24),
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.widthIn(8.dp))
                 Text(text = stringResource(id = MR.strings.setup_open_browser.resourceId))
             }
         }
         FloatingActionButton(
             onClick = { /*TODO*/ },
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.primary,
             modifier = Modifier
                 .align(BottomEnd)
                 .padding(32.dp)
-        ) {}
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_arrow_forward_24),
+                contentDescription = stringResource(id = MR.strings.setup_continue.resourceId)
+            )
+        }
     }
 }
