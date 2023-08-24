8eedc71612ee8ebf5f4a459f148f61ddad81a5d1
Vaclav Drahorad <drahorad.vac@gmail.com>
2023-08-24 21:15:07 +0200

    New release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.drahovac.weatherstationdisplay.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.2.2-alpha"
+        versionCode = 6
+        versionName = "1.2.3-alpha"
     }
     buildFeatures {
         compose = true
